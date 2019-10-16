c1c0099d4e97046205ecc668e7a1ebaccc63b9e7
TakuyaYoshimatsu <55725098+TakuyaYoshimatsu@users.noreply.github.com>
2019-10-16 22:51:57 +0900

    takuya

--- Mammoner/Assets/Hero/Script/SearchCharacter.cs
+++ Mammoner/Assets/Hero/Script/SearchCharacter.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class SearchCharacter : MonoBehaviour
 {
+    private MoveHero moveHero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveHero = GetComponentInParent<MoveHero>();
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
         if(col.tag == "Monster"){
-            Debug.Log("★"); 
+            //　敵キャラクターの状態を取得
+            MoveHero.HeroState state = moveHero.GetState();
+            if(state != MoveHero.HeroState.Chase)
+            {
+                moveHero.SetState(MoveHero.HeroState.Chase, col.transform);
+            }
         }
     }
 }
