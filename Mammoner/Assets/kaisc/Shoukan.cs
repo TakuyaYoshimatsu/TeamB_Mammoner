d93c709caa959eb8b09799bf3196bd37a00ff0ea
AlterboKyaida <yusuke.kaida.alt@gmail.com>
2019-10-25 14:21:54 +0900

    UI調整

--- Mammoner/Assets/kaisc/Shoukan.cs
+++ Mammoner/Assets/kaisc/Shoukan.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-
+using UnityEngine.UI;
 
 //モンスターを召還するメイン処理
 public class Shoukan : MonoBehaviour
@@ -13,15 +12,21 @@ public class Shoukan : MonoBehaviour
 	//0:スライム 1:ゴブリン 3:罠 4:空のオブジェクトを指定することを前提としている
 	int arrayNumber = 3;
 
-	//召還者（マモナー）のMP
-	int mamonerMp = 1000;
+    //召還者（マモナー）のMP
+    private int mamonerMp = 100;
 
-	//MP消費量
-	int cost;
+    //MP消費量
+    int cost;
+
+    //召還者（マモナー）のMPをテキスト表示
+    private Text mamonerMpText;
+
+    //勝利テキスト
+    private Text syouriText;
 
 
-	//スライムを選択
-	public void Srime()
+    //スライムを選択
+    public void Srime()
 	{
 		cost = 5;
 		//arrayNumberの設定
@@ -75,38 +80,56 @@ public class Shoukan : MonoBehaviour
 
 	private void Update()
 	{
-		//PointerEventData pointer = new PointerEventData(EventSystem.current);
-		//変更する
-		//pointer.position = Input.mousePosition;
-		//List<RaycastResult> result = new List<RaycastResult>();
-		//EventSystem.current.RaycastAll(pointer, result);
-		//boolean eleaFlag = true;
-		//if (raycastResult.gameObject.name != "UI"){flag = ture;}else{flag = false}
 
-		//TODO もし選択エリアでない場合の分岐を追加
-		//MPがから０でない場合、左クリックした位置に召還する
-		if (mamonerMp != 0 && Input.GetMouseButtonDown(1) )
+        if (mamonerMp <= 0)
+        {
+            //Textオブジェクトをキャッシュ
+            syouriText = GameObject.Find("SYOURIText").GetComponent<Text>();
+            //現在HPを画面に表示
+            syouriText.text = "勝利";
+        }
+
+        //PointerEventData pointer = new PointerEventData(EventSystem.current);
+        //変更する
+        //pointer.position = Input.mousePosition;
+        //List<RaycastResult> result = new List<RaycastResult>();
+        //EventSystem.current.RaycastAll(pointer, result);
+        //boolean eleaFlag = true;
+        //if (raycastResult.gameObject.name != "UI"){flag = ture;}else{flag = false}
+
+        //TODO もし選択エリアでない場合の分岐を追加
+        //MPがから０でない場合、左クリックした位置に召還する
+        if (mamonerMp > 0 && Input.GetMouseButtonDown(1) )
 		{
 			//TODO
 			//プレイヤーの現在位置　後々変更。スマホでタップした位置に変える。
-			Vector3 syoukanPosision = transform.position;
-			//Vector3 mousePos = Input.mousePosition;
-			//mousePos.z = 10.0f;
-			//Vector3 syoukanPosision = Camera.main.ScreenToWorldPoint(mousePos);
+			//Vector3 syoukanPosision = transform.position;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10.0f;
+            Vector3 syoukanPosision = Camera.main.ScreenToWorldPoint(mousePos);
 
-			//タップした位置の座標
-			//Vector3 touchPosition = touch.position;
-			//Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            //タップした位置の座標
+            //Vector3 touchPosition = touch.position;
+            //Vector3 syoukanPosision = Camera.main.ScreenToWorldPoint(touchPosition);
 
-			//マウスの位置
-			//Vector3 syoukanPosision = Input.mousePosition;
+            //マウスの位置
+            //Vector3 syoukanPosision = Input.mousePosition;
 
-			//召還処理
-			Instantiate(prefabs[arrayNumber], syoukanPosision, Quaternion.identity);
+            //召還処理
+            Instantiate(prefabs[arrayNumber], syoukanPosision, Quaternion.identity);
 
 			//召還コスト分MPを差し引く
 			mamonerMp -= cost;
-		}
+
+            //Textオブジェクトをキャッシュ
+            mamonerMpText = GameObject.Find("Health Text").GetComponent<Text>();
+            //現在HPを画面に表示
+            mamonerMpText.text = "MP:" + mamonerMp.ToString();
+
+ 
+
+
+        }
 
 		//気絶時の行動　仮実装したが、不要となった機能
 		if (IsStan())
@@ -117,6 +140,7 @@ public class Shoukan : MonoBehaviour
 			recoverTime -= Time.deltaTime;
 
 		}
+
 	}
 
 	//罠に当たった時の判定　不要となった機能
@@ -136,5 +160,24 @@ public class Shoukan : MonoBehaviour
 			Destroy(hit.gameObject);
 		}
 
-	}
+
+
+    }
+
+    
+   
+    void Start()
+    {
+        if (mamonerMp > 0)
+        {
+            //Textオブジェクトをキャッシュ
+            mamonerMpText = GameObject.Find("Health Text").GetComponent<Text>();
+            //現在HPを画面に表示
+            mamonerMpText.text = "MP:" + mamonerMp.ToString();
+
+
+        }
+
+    }
+
 }
